688622b38fcd8b2e752b5abb2016f69ad09963bf
Asmaa <Asmaa99musse@gmail.com>
2021-12-20 06:06:17 +0000

    created while if statement for role

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const inquirer = require("inquirer");
 const Db = require("./db/db");
 
-const { getDepartments, getRole, getEmployee } = require("./utils/getTables");
+const {
+  selectFromDepartment,
+  selectFromRole,
+  selectFromEmployee,
+} = require("./utils/selectAllFrom");
+
 const {
   initialQuestions,
   departmentQuestions,
@@ -32,11 +37,61 @@ const start = async () => {
     const { option } = await initialQuestions(inquirer);
     console.log(option);
 
+    // Add/View Department
     if (option === "addDepartment") {
       const { departmentName } = await departmentQuestions(inquirer);
       const query = `INSERT INTO department (name) VALUES ('${departmentName}');`;
       await db.query(query);
       console.log(`Added ${departmentName} into database`);
+    }
+    if (option === "viewDepartment") {
+      const data = await selectFromDepartment(db);
+      console.table(data);
+    }
+
+    // Add/View Role
+    if (option === "addRole") {
+      const departments = await selectFromDepartment(db);
+
+      if (departments.length) {
+        const questions = await roleQuestions(db);
+        const { name, salary, department_id } = await inquirer.prompt(
+          questions
+        );
+
+        const query = `INSERT INTO role (title, salary, department_id) VALUES ("${name}", "${salary}", "${department_id}");`;
+        await db.query(query);
+        console.log(`Added ${name} into Role Table!`);
+      } else {
+        console.warn("[ERROR]: Please enter a department before proceeding...");
+      }
+    }
+    if (option === "viewRole") {
+      const data = await selectFromRole(db);
+      console.table(data);
+    }
+
+    if (option === "addEmployee") {
+      const role = await selectFromRole(db);
+
+      if (role.length) {
+        const questions = await employeeQuestions(db);
+        const { firstName, lastName, role_id } = await inquirer.prompt(
+          questions
+        );
+
+        const query = `INSERT INTO employee (first_name, last_name, role_id) VALUES ("${firstName}","${lastName}","${role_id}");`;
+        await db.query(query);
+        console.log(`Created ${firstName} ${lastName} as an employee!`);
+      } else {
+        console.warn("[ERROR]: Please enter a role before proceeding...");
+      }
+    }
+    if (option === "viewEmployee") {
+      const query =
+        'SELECT CONCAT(employee_role.first_name, " ", employee_role.last_name) AS Employee, title as "Role", salary as "Salary",  name as "Department",  CONCAT (employee_manager.first_name, " ", employee_manager.last_name) as "Manager"  FROM employee employee_role LEFT JOIN role ON employee_role.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee employee_manager ON employee_role.manager_id = employee_manager.id;';
+      const data = await db.query(query);
+      console.table(data);
     }
   }
 
